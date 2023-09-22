305ddca6b791e56dc102dda7185c9b0f8433da06
Justin Moreira <justinmoreira23@gmail.com>
2023-09-22 16:39:11 -0400

    Display form content onto test display

--- function.js
+++ function.js
@@ -1,23 +1,40 @@
 const myLibrary = [];
 
-class Book {
-    constructor(name, author, pages, read) {
-        this.name = name;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
+function Book (name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+
+    let name = document.getElementById('book-name').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('book-pages').value;
+    let read = document.getElementById('been-read').value;
+
+    const book = new Book(name, author, pages, read);
+    
+    myLibrary.push(book);
+
+    displayBooks();
+}
+
+function displayBooks(){
+    for(let i = 0; i < myLibrary.length; i++){
+        document.getElementById('input-title').textContent = myLibrary[i].name;
+        document.getElementById('input-author').textContent = myLibrary[i].author;
+        document.getElementById('input-pages').textContent = myLibrary[i].pages;
+        document.getElementById('input-read').textContent = myLibrary[i].read;
     }
 }
 
-function addBookToLibrary(name, author, pages, read) {
-    myLibrary.push(Book(name, author, pages, read))
+function logSubmit(event){
+    event.preventDefault();
 }
 
-function displayBooks(arr){
-    for(let i = 0; i < arr.length; i++){
-        arr[i].name;
-        arr[i].author;
-        arr[i].pages;
-        arr[i].read;
-    }
-}
+const form = document.getElementById('form');
+form.addEventListener("submit", logSubmit);
+form.addEventListener("submit", addBookToLibrary);
+
